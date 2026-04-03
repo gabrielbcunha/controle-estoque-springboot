69a42575de333a7a4f166463fdf8b5b7207d6b3b
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-04-03 17:27:47 -0300

    First Commit

---
+++ src/main/java/br/com/gabrielbcunha/controleetoquespringboot/ControleEtoqueSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielbcunha.controleetoquespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEtoqueSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleEtoqueSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gabrielbcunha/controleetoquespringboot/ControleEtoqueSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielbcunha.controleetoquespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEtoqueSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

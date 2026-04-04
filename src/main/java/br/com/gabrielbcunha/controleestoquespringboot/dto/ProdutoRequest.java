06593917ad280d2008d43c635a0d9f2021cca95f
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-04-03 21:39:32 -0300

    feat(dto): cria DTO do tipo Request para Produto

---
+++ src/main/java/br/com/gabrielbcunha/controleestoquespringboot/dto/ProdutoRequest.java
@@ -0,0 +1,9 @@
+package br.com.gabrielbcunha.controleestoquespringboot.dto;
+
+import java.math.BigDecimal;
+
+public class ProdutoRequest {
+    private String nome;
+    private BigDecimal preco;
+    private int quantidade;
+}

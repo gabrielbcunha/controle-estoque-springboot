24fdbf88ece41b5231ea08a8488fbec4518949b1
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-04-03 21:48:18 -0300

    feat(service): cria o service ProdutoService com validações de regras de negócio

---
+++ src/main/java/br/com/gabrielbcunha/controleestoquespringboot/service/ProdutoService.java
@@ -0,0 +1,39 @@
+package br.com.gabrielbcunha.controleestoquespringboot.service;
+
+import br.com.gabrielbcunha.controleestoquespringboot.dto.ProdutoRequest;
+import br.com.gabrielbcunha.controleestoquespringboot.entity.Produto;
+import br.com.gabrielbcunha.controleestoquespringboot.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    public Produto adicionarProduto(ProdutoRequest dadosDoProduto) {
+
+        if (dadosDoProduto.getNome() == null || dadosDoProduto.getNome().isBlank()) {
+            throw new IllegalArgumentException("O nome do Produto deve existir");
+        }
+        if (dadosDoProduto.getPreco() == null) {
+            throw new IllegalArgumentException("O preço do Produto deve existir");
+        }
+        if (dadosDoProduto.getQuantidade() < 0) {
+            throw new IllegalArgumentException("A quantidade do Produto deve ser positiva");
+        }
+
+        String nome = dadosDoProduto.getNome();
+        BigDecimal preco = dadosDoProduto.getPreco();
+        int quantidade = dadosDoProduto.getQuantidade();
+
+        Produto novoProduto = new Produto(null, nome, preco, quantidade);
+        Produto produtoSalvo = produtoRepository.save(novoProduto);
+        return produtoSalvo;
+    }
+}
